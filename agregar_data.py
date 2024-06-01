2dcd79137dd9536419993ee723a18185e1824ee2
sordonez1983 <sebastian.ordz@gmail.com>
2024-05-31 23:42:56 -0500

    actualizacion_bdd

--- agregar_data.py
+++ agregar_data.py
@@ -15,15 +15,22 @@ session = Session()
 local1 = LocalesComida(nombre="El Sabor Ecuatoriano", tipo_comida="Ecuatoriana", direccion="Av. 10 de Agosto y 12 de Octubre", horario_atencion="Lunes a Domingo: 10am - 10pm", calificacion=5)
 local2 = LocalesComida(nombre="Pizza Italia", tipo_comida="Italiana", direccion="Calle Rocafuerte y Bolivar", horario_atencion="Martes a Sabado: 12pm - 11pm", calificacion=4)
 local3 = LocalesComida(nombre="Sushi Wok", tipo_comida="Asiática", direccion="Av. Amazonas y Eloy Alfaro", horario_atencion="Miércoles a Domingo: 1pm - 12am", calificacion=4)
+local4 = LocalesComida(nombre="El Mariachi", tipo_comida="Mexicana", direccion="Av. Patria y López Mateos", horario_atencion="Todos los días: 11am - 11pm", calificacion=4.5)
+local5 = LocalesComida(nombre="Taj Mahal", tipo_comida="India", direccion="Calle Morelos y Madero", horario_atencion="Lunes a Sábado: 12pm - 10pm", calificacion=5)
+local6 = LocalesComida(nombre="Le Croissant", tipo_comida="Francesa", direccion="Av. Insurgentes y Reforma", horario_atencion="Miércoles a Domingo: 7pm - 1am", calificacion=4.2)
+
 
 # Example centro deportivo data
 centro1 = CentrosDeportivos(nombre="Gimnasio Power", tipo_deporte="Musculación y Cardio", direccion="Av. Patria Nueva y 6 de Diciembre", horario_atencion="Lunes a Viernes: 6am - 10pm, Sábados: 8am - 1pm", costo_membresia=50)
 centro2 = CentrosDeportivos(nombre="Piscina Olímpica", tipo_deporte="Natación", direccion="Calle Cumandá y Veintimilla", horario_atencion="Lunes a Domingo: 7am - 9pm", costo_membresia=30)
 centro3 = CentrosDeportivos(nombre="Complejo Deportivo Los Olivos", tipo_deporte="Fútbol, Tenis, Baloncesto", direccion="Av. Simón Bolívar y Mariscal Sucre", horario_atencion="Lunes a Domingo: 8am - 10pm",costo_membresia=20)
+centro4 = CentrosDeportivos(nombre="Gimnasio Power", tipo_deporte="Musculación y Cardio", direccion="Av. Patria Nueva y 6 de Diciembre", horario_atencion="Lunes a Viernes: 6am - 10pm, Sábados: 8am - 1pm", costo_membresia=50)
+centro5 = CentrosDeportivos(nombre="Piscina Olímpica", tipo_deporte="Natación", direccion="Calle Cumandá y Veintimilla", horario_atencion="Lunes a Domingo: 7am - 9pm", costo_membresia=30)
+centro6 = CentrosDeportivos(nombre="Complejo Deportivo Los Olivos", tipo_deporte="Fútbol, Tenis, Baloncesto", direccion="Av. Simón Bolívar y Mariscal Sucre", horario_atencion="Lunes a Domingo: 8am - 10pm", costo_membresia=20)
 
 # Add cities and stadiums to the session
-session.add_all([local1, local2, local3])
-session.add_all([centro1, centro2, centro3])
+session.add_all([local1, local2, local3, local4, local5, local6])
+session.add_all([centro1, centro2, centro3,centro4,centro5,centro6])
 
 try:
     session.commit()
--- base_datos.py
+++ base_datos.py
@@ -2,14 +2,14 @@ import os
 from sqlalchemy import create_engine
 
 # Check database file existence
-if not os.path.exists('localesComida_centrosDeportivos.db'):
-  print("Error: El archivo de base de datos 'localesComida_centrosDeportivos.db' no existe.")
+if not os.path.exists('Listado_de_establecimientos_de_comida_centros deportivos.db'):
+  print("Error: El archivo de base de datos 'Listado_de_establecimientos_de_comida_centros deportivos.db' no existe.")
   exit(1)
 
 
 # Create engine with connection options
 engine = create_engine(
-    'sqlite:///localesComida_centrosDeportivos.db',
+    'sqlite:///Listado_de_establecimientos_de_comida_centros deportivos.db',
     pool_size=5,
     connect_args={'check_same_thread': False}
 )
