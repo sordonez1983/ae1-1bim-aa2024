29bdcc2e9279bd176c9c910a0d7261b83523d303
sordonez1983 <sebastian.ordz@gmail.com>
2024-06-04 20:57:45 -0500

    Recuperación Nota

--- agregar_data.py
+++ agregar_data.py
@@ -12,10 +12,10 @@ session = Session()
 # Ejemplo de agregar data cuenta de ahorro
 
 cuenta1 = CuentasAhorro(numero_cuenta="CA12345", nombre_titular="Juan Pérez", saldo_actual=1000, fecha_apertura=datetime.date(year=2024, month=6, day=4))
-cuenta2 = CuentasAhorro(numero_cuenta="CA56789", nombre_titular="María Gómez", saldo_actual=500, fecha_apertura=datetime.date(year=2023, month=12, day=2))
-cuenta3 = CuentasAhorro(numero_cuenta="CA01257", nombre_titular="Genny Alcivar", saldo_actual=1000, fecha_apertura=datetime.date(year=2022, month=5, day=7))
-cuenta4 = CuentasAhorro(numero_cuenta="CA06036", nombre_titular="Aníbal Sánchez", saldo_actual=500, fecha_apertura=datetime.date(year=2021, month=11, day=12))
-cuenta5 = CuentasAhorro(numero_cuenta="CA54377", nombre_titular="Rosa Guzmán", saldo_actual=1000, fecha_apertura=datetime.date(year=2000, month=8, day=7))
+cuenta2 = CuentasAhorro(numero_cuenta="CA56789", nombre_titular="María Gómez", saldo_actual=300, fecha_apertura=datetime.date(year=2023, month=12, day=2))
+cuenta3 = CuentasAhorro(numero_cuenta="CA01257", nombre_titular="Genny Alcivar", saldo_actual=650, fecha_apertura=datetime.date(year=2022, month=5, day=7))
+cuenta4 = CuentasAhorro(numero_cuenta="CA06036", nombre_titular="Aníbal Sánchez", saldo_actual=740, fecha_apertura=datetime.date(year=2021, month=11, day=12))
+cuenta5 = CuentasAhorro(numero_cuenta="CA54377", nombre_titular="Rosa Guzmán", saldo_actual=1250, fecha_apertura=datetime.date(year=2000, month=8, day=7))
 cuenta6 = CuentasAhorro(numero_cuenta="CA17125", nombre_titular="María Vera", saldo_actual=500, fecha_apertura=datetime.date(year=1999, month=10, day=28))
 
 session.add_all([cuenta1, cuenta2, cuenta3, cuenta4, cuenta5, cuenta6])
